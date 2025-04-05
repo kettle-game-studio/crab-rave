d4ff800957d368655c6262e7668a2ad7e43f0479
Tetrergeru <ArutunoffOleg@yandex.ru>
2025-04-05 16:47:20 +0200

    Add player prefab and gamedesign file

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public GameObject verticalPivot;
+    public GameObject horizontalPivot;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
